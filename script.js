b596cd7032d588582d537df49b66a6f4511ef9b3
cutebid <67562053+cutebid@users.noreply.github.com>
2021-08-09 00:09:30 +0100

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
         const toggle_button = () => {
-                   let hamburger = document.querySelector('.toggleBtn');
                    let search = document.querySelector('.form')
                    console.log(search)
                    search.classList.toggle('active')
